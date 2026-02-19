78bc1853fdb467613178ed615ed8e91000ad5dcb
Denyse Freitas Santos <denysefreitass@gmail.com>
2026-02-19 09:06:45 -0300

    feat(exercicios-javaprogressivo): resolver exemplos e exercícios básicos de orientação a objetos, parte III: interfaces

---
+++ src/javaprogressivo/pooparteiii/listainterface/exemplos/domain/Matriz.java
@@ -0,0 +1,7 @@
+package javaprogressivo.pooparteiii.listainterface.exemplos.domain;
+
+public interface Matriz {
+    double Exibir(Matriz m);
+    Matriz Anular(int linha, int coluna);
+    Matriz AdicionarAsterisco(Matriz m);
+}
---
+++ src/javaprogressivo/pooparteiii/listainterface/exemplos/domain/minhaMatriz.java
@@ -0,0 +1,22 @@
+package javaprogressivo.pooparteiii.listainterface.exemplos.domain;
+
+public class minhaMatriz implements Matriz{
+    Matriz matriz;
+    double traco;
+
+    @Override
+    public double Exibir(Matriz m) {
+        // Exibir matriz
+        return traco;
+    }
+
+    @Override
+    public Matriz Anular(int linha, int coluna) {
+        return matriz;
+    }
+
+    @Override
+    public Matriz AdicionarAsterisco(Matriz m) {
+        return matriz;
+    }
+}
